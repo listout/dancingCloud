12816ac1decf7841821d4d70ae8c20a4ee77b8eb
listout <listout@protonmail.com>
2022-02-09 11:52:07 +0530

    Formatting

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,9 +2,9 @@
 
 const int N = 256; //Number of bands in the spectrum
 float spectrum[N]; //Smoothed spectrum values
-float Rad   = 500; //Cloud radius parameter
-float Vel   = 0.1; //Cloud points velocity
-int bandRad = 2;   //Band Index in spectrum, affecting Rad value
+float Rad = 500; //Cloud radius parameter
+float Vel = 0.1; //Cloud points velocity
+int bandRad = 2; //Band Index in spectrum, affecting Rad value
 int bandVel = 100; //Band index in spectrum, affecting Vel value
 
 const int n = 300; //Number of cloud points
@@ -16,29 +16,26 @@ ofPoint p[n]; //Cloud points position
 float time0 = 0; //Time value, use for dt computation
 
 unsigned long trackNum = 0; //the track number
-bool haveDirectory;         // flag to check if we have directory as arguments
+bool haveDirectory; // flag to check if we have directory as arguments
 
 //--------------------------------------------------------------
-void
-ofApp::setup()
+void ofApp::setup()
 {
 	//setting the directory path
 	//if not directory then play the same file on loop
 	string path = arguments[1];
 	dir.open(path);
 	//Check if a valid directory and set up the sound
-	if(dir.exists())
-	{
+	if (dir.exists()) {
 		haveDirectory = true;
 		//only read mp3 files
 		dir.allowExt("mp3");
 		//populate the directory object
 		dir.listDir();
 		dir.sort();
-		sound.load(dir.getPath(trackNum++)); //play the first track from the directory list
-	}
-	else
-	{
+		sound.load(dir.getPath(
+			trackNum++)); //play the first track from the directory list
+	} else {
 		sound.load(path);
 		sound.setLoop(true);
 	}
@@ -47,22 +44,19 @@ ofApp::setup()
 	sound.play();
 
 	//Set spectrum values to 0
-	for(int i = 0; i < N; ++i)
-	{
+	for (int i = 0; i < N; ++i) {
 		spectrum[i] = 0.0f;
 	}
 
 	//Initialize points offset by random number
-	for(int j = 0; j < n; ++j)
-	{
+	for (int j = 0; j < n; ++j) {
 		tx[j] = ofRandom(0, 1000);
 		ty[j] = ofRandom(0, 1000);
 	}
 }
 
 //--------------------------------------------------------------
-void
-ofApp::update()
+void ofApp::update()
 {
 	//Update sound engine
 	ofSoundUpdate();
@@ -73,8 +67,7 @@ ofApp::update()
 	//Update the smoothed spectrum
 	//by slowly decreasing the values and getting maximum value
 	//to have slowly falling peaks in the spectrum
-	for(int i = 0; i < N; ++i)
-	{
+	for (int i = 0; i < N; ++i) {
 		spectrum[i] *= 0.97;
 		spectrum[i] = max(spectrum[i], val[i]);
 	}
@@ -84,17 +77,16 @@ ofApp::update()
 	//Computing dt as time between the last and the current
 	//calling of update()
 	float time = ofGetElapsedTimef();
-	float dt   = time - time0;
-	dt         = ofClamp(dt, 0.0, 0.1);
-	time0      = time;
+	float dt = time - time0;
+	dt = ofClamp(dt, 0.0, 0.1);
+	time0 = time;
 
 	//Updating Rad and vel from the spectrum
 	Rad = ofMap(spectrum[bandRad], 1, 3, 400, 800, true);
 	Vel = ofMap(spectrum[bandVel], 0, 0.1, 0.05, 0.5);
 
 	//Update particle position
-	for(int j = 0; j < n; ++j)
-	{
+	for (int j = 0; j < n; ++j) {
 		tx[j] += Vel * dt; //move offset
 		tx[j] += Vel * dt; //move offset
 
@@ -106,15 +98,14 @@ ofApp::update()
 }
 
 //--------------------------------------------------------------
-void
-ofApp::draw()
+void ofApp::draw()
 {
-
-	if(haveDirectory && (sound.isPlaying() == false || sound.getPosition() == 1.0))
-	{
-		if(trackNum == dir.size()) trackNum = 0; //reset track number if at last file
-		sound.unload();                          //unload any previous file
-		sound.load(dir.getPath(trackNum++));     // load the next track
+	if (haveDirectory &&
+	    (sound.isPlaying() == false || sound.getPosition() == 1.0)) {
+		if (trackNum == dir.size())
+			trackNum = 0; //reset track number if at last file
+		sound.unload(); //unload any previous file
+		sound.load(dir.getPath(trackNum++)); // load the next track
 		sound.play();
 	}
 
@@ -127,16 +118,12 @@ ofApp::draw()
 
 	//Draw spectrum
 	ofSetColor(46, 49, 61);
-	for(int i = 0; i < N; ++i)
-	{
+	for (int i = 0; i < N; ++i) {
 		//Draw bandRad and bandVel by black color
 		//and any other by gray color
-		if(i == bandRad || i == bandVel)
-		{
+		if (i == bandRad || i == bandVel) {
 			ofSetColor(161, 141, 175);
-		}
-		else
-		{
+		} else {
 			ofSetColor(198, 174, 215);
 		}
 		ofDrawRectangle(10 + i * 5, 600, 3, -spectrum[i] * 100);
@@ -146,24 +133,20 @@ ofApp::draw()
 
 	//Move centre of coordinate system to the screen center
 	ofPushMatrix();
-	ofTranslate(ofGetWidth() / 2, ofGetHeight() / 2);
+	ofTranslate(ofGetWidth() / 2., ofGetHeight() / 2.);
 
 	//Draw cloud's points
 	ofSetColor(181, 180, 201);
 	ofFill();
-	for(int i = 0; i < n; ++i)
-	{
+	for (int i = 0; i < n; ++i) {
 		ofDrawCircle(p[i], 2);
 	}
 
 	//Draw lines between near points
 	float dist = 40; //Threshold parameter of distance
-	for(int j = 0; j < n; ++j)
-	{
-		for(int k = 0; k < n; ++k)
-		{
-			if(ofDist(p[j].x, p[j].y, p[k].x, p[k].y) < dist)
-			{
+	for (int j = 0; j < n; ++j) {
+		for (int k = 0; k < n; ++k) {
+			if (ofDist(p[j].x, p[j].y, p[k].x, p[k].y) < dist) {
 				ofDrawLine(p[j], p[k]);
 			}
 		}
@@ -174,67 +157,56 @@ ofApp::draw()
 }
 
 //--------------------------------------------------------------
-void
-ofApp::keyPressed(int key)
+void ofApp::keyPressed(int key)
 {
 }
 
 //--------------------------------------------------------------
-void
-ofApp::keyReleased(int key)
+void ofApp::keyReleased(int key)
 {
 }
 
 //--------------------------------------------------------------
-void
-ofApp::mouseMoved(int x, int y)
+void ofApp::mouseMoved(int x, int y)
 {
 }
 
 //--------------------------------------------------------------
-void
-ofApp::mouseDragged(int x, int y, int button)
+void ofApp::mouseDragged(int x, int y, int button)
 {
 }
 
 //--------------------------------------------------------------
-void
-ofApp::mousePressed(int x, int y, int button)
+void ofApp::mousePressed(int x, int y, int button)
 {
 }
 
 //--------------------------------------------------------------
-void
-ofApp::mouseReleased(int x, int y, int button)
+void ofApp::mouseReleased(int x, int y, int button)
 {
 }
 
 //--------------------------------------------------------------
-void
-ofApp::mouseEntered(int x, int y)
+void ofApp::mouseEntered(int x, int y)
 {
 }
 
 //--------------------------------------------------------------
-void
-ofApp::mouseExited(int x, int y)
+void ofApp::mouseExited(int x, int y)
 {
 }
 
 //--------------------------------------------------------------
-void
-ofApp::windowResized(int w, int h)
+void ofApp::windowResized(int w, int h)
 {
 }
 
 //--------------------------------------------------------------
-void
-ofApp::gotMessage(ofMessage msg)
+void ofApp::gotMessage(ofMessage msg)
 {
 }
 
 //--------------------------------------------------------------
-void
-ofApp::dragEvent(ofDragInfo dragInfo)
+void ofApp::dragEvent(ofDragInfo dragInfo)
 {
 }
